4173d58d34320bfb1dafa6c1f883f8052fd18abb
Susam Pal <susam@susam.net>
2026-04-22 06:34:01 +0100

    Create distributable bundle for Wander Console 0.6.0.dev2

--- dev/examples/code-injection-console/wander.js
+++
@@ -1,7 +0,0 @@
-const wander = { // eslint-disable-line no-unused-vars
-  _checkURLs: false,
-  consoles: ['http://localhost:8000/dev/examples/code-injection-console/mallory/'],
-  pages: ['https://example.com/']
-}
-document.cookie = 'name=alice'
-console.log('Alice reads cookie:', document.cookie)
--- dev/examples/message-injection-page/wander.js
+++
@@ -1,4 +0,0 @@
-const wander = { // eslint-disable-line no-unused-vars
-  _checkURLs: false,
-  pages: ['page.html']
-}
