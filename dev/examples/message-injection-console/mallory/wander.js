9d569987b14f15476b6a72cebd363159e237a8b5
Susam Pal <susam@susam.net>
2026-05-08 21:12:46 +0100

    Create distributable bundle for Wander Console 0.6.0

--- dev/examples/message-injection-console/mallory/wander.js
+++
@@ -1,3 +0,0 @@
-/* global parent */
-const wander = {} // eslint-disable-line no-unused-vars
-parent.postMessage("Hello from Mallory's console", '*')
--- dev/examples/message-injection-console/wander.js
+++
@@ -1,4 +0,0 @@
-const wander = { // eslint-disable-line no-unused-vars
-  consoles: ['http://localhost:8000/dev/examples/message-injection-console/mallory/'],
-  pages: ['https://example.com/']
-}
